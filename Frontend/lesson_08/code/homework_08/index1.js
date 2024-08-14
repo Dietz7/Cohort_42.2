59808d54728dee6a33111d7db09f912a22ca6af7
Your Name <you@example.com>
2024-08-14 21:14:42 +0200

    code/homework_08/index1.js

---
+++ Frontend/lesson_08/code/homework_08/index1.js
@@ -0,0 +1,57 @@
+const a = 17;
+const b = 5;
+console.log('a > b is ' + (a > b));
+console.log('a <= b is ' + (a <=b));
+console.log('a == b is ' + (a==b));
+console.log('a != b is ' + (a !=b));
+console.log('a' < 'b'); // true a is before b
+console.log(('ab' > 'a')); // true beacuse ab is longer than a
+console.log("'17 > 1:", '17' > 1);
+console.log(17===1); //false
+console.log(17===17); // true
+console.log(17==='17'); //false
+console.log(17===true); //false
+console.log('0' === ''); //false
+console.log(true===false); //false
+console.log(true===true); //true
+console.log(null ===undefined);//false
+console.log(false===0);//false
+console.log(17=='17'); //true
+console.log("'0'" == ''); //false
+console.log(0 == '');//true
+console.log(null == undefined); //true
+console.log(false ==0);
+
+console.log(undefined == null);//true
+console.log(undefined == 0); // false
+console.log(null == 0); // false
+console.log(undefined < 0);// false
+console.log(undefined > 0); // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
