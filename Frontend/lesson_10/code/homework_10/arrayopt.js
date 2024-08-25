596f92fed2bffe55e44e2db54900023b3713aa77
Your Name <you@example.com>
2024-08-25 20:06:26 +0200

    updated array.opt.js

--- Frontend/lesson_10/code/homework_10/arrayopt.js
+++ Frontend/lesson_10/code/homework_10/arrayopt.js
@@ -2,5 +2,19 @@
 //Find the maximum and minimum values from the array [3, 7, 2, 9, 4] and display them in the console. ! 
 //Do not use max and min math methods, only loops
 
-let arr = [3, 7, 2, 9, 4];
+let arrayNumbers = [3, 7, 2, 9, 4];
+
+let max = arrayNumbers[0];
+let min = arrayNumbers[0];
+
+for(let number of arrayNumbers){
+if(number > max){
+    max = number;
+}
+if(number < min){
+min = number;
+}
+}
+console.log(`Max - ${max}`);
+console.log(`Min - ${min}`);
 
