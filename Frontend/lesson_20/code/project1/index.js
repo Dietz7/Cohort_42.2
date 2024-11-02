1009d70fb0e00c1d82ab3986a5f3da93d836a67f
Dietz7 <yuliya.arkhi@gmail.com>
2024-11-02 20:11:47 +0100

    added lesson_20

---
+++ Frontend/lesson_20/code/project1/index.js
@@ -0,0 +1,45 @@
+
+const cityInput = document.querySelector(".cityInput");
+const getWeatherButton = document.querySelector("#get-weather-button");
+const displayWeatherContainer = document.querySelector(".display-weather-container");
+const API_KEY = "5e77196db6dc9bd44d2d40b708ff59df";
+
+const getWeatherData = async () => {
+    const city = cityInput.value.trim();
+    if (!city) {
+        alert("Please input the city");
+        return;
+    }
+
+    displayWeatherContainer.innerHTML = "<p>Loading...</p>";
+    displayWeatherContainer.classList.remove("hidden");
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=en`);
+        const weatherData = await response.json(); 
+        if (response.ok) {
+            // Only if response is okay
+            const roundedTemp = Math.round(weatherData.main.temp);
+            const roundedWind = Math.round(weatherData.wind.speed);
+            displayWeatherContainer.innerHTML = `
+                <h2>${weatherData.name}</h2>
+                <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png" alt="Weather Icon" />
+                <p>${roundedTemp}°C</p>
+                <p>${weatherData.weather[0].description}</p>
+                <p>Wind: ${roundedWind} m/s</p>
+            `;
+        } else {
+            throw Object.assign(new Error("Error fetching weather data"), {
+                responseError: weatherData, 
+            });
+        }
+    } catch (error) {
+        console.log(error.responseError); // Log the error details for debugging
+        displayWeatherContainer.innerHTML = `<p class="error">${error.responseError.cod}: ${error.responseError.message}</p>`;
+        displayWeatherContainer.classList.remove("hidden");
+    } finally {
+        displayWeatherContainer.classList.remove("hidden");
+    }
+};
+
+getWeatherButton.addEventListener('click', getWeatherData);
